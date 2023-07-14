2228b9d5412648fd4fff870fa0af877a43189518
Ebihara Yusuke <yusuke@ebihara.me>
2023-07-14 14:19:49 +0900

    update keymap

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/ebiyuu1121/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/ebiyuu1121/keymap.c
@@ -23,9 +23,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
-    KC_ESC   , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                  KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_MINS  ,
-    KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_INT3  ,
-    KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , S(KC_7)  ,
+    KC_ESC   , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                  KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_EQL   ,
+    KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_BSLS  ,
+    KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_LBRC  ,            KC_RBRC  , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , MO(3)    ,
     _______  , _______  , KC_LALT  , KC_LGUI  ,MO(1), LT(2,KC_SPC), LT(3,KC_ESC),           KC_BSPC  , KC_ENT   , _______  , _______  , _______  , _______  , KC_MINS
   ),
@@ -35,15 +35,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , KC_7     , KC_8     , KC_9     , _______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , KC_4     , KC_5     , KC_6     , _______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , KC_0     , KC_1     , KC_2     , KC_3     , KC_DOT   , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  , _______  , _______  , _______  , _______  , _______
+    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            KC_BSPC  , KC_ENT   , _______  , _______  , _______  , _______  , _______
   ),
 
   [2] = LAYOUT_universal(
-    _______  , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                  KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
+    _______  , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                  KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   , 
     _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , _______  , _______  , _______  , _______  , KC_F12   ,
     _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , KC_BTN1  , KC_BTN3  , KC_BTN2  , _______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  , _______  ,             _______ , _______  ,RCS(KC_TAB), _______ , C(KC_TAB), _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,             KC_DEL  , KC_ENT   , _______  , _______  , _______  , _______  , KC_GRAVE
+    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,             KC_BTN4 , KC_BTN5  , _______  , _______  , _______  , _______  , KC_GRAVE
   ),
 
   [3] = LAYOUT_universal(
